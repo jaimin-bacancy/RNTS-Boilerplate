d8ad6b20688d73f49b5781be25e926a464e0d39b
Jaimin Bhut <jaimin.bhut@bacancy.com>
2023-09-05 09:44:45 +0530

    feat: add build flavor/scheme and env support

--- src/screens/SignIn/SignIn.tsx
+++ src/screens/SignIn/SignIn.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import Config from 'react-native-config';
 import styles from './SignIn.styles';
 
 export function SignIn(): JSX.Element {
   return (
     <View style={styles.container}>
-      <Text>HOLA</Text>
+      <Text>{Config.TARGET}</Text>
     </View>
   );
 }
